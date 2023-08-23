5707877880ae3ae8b9edddf6e589af9066696a0e
Thor Anderson <ta.anderson@gmail.com>
2023-08-23 16:25:16 -0600

    Just testing things

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
-		<h2 class="h2">Welcome to Skeleton.</h2>
+		<h2 class="h2">Welcome to my Starter Svelte Project!</h2>
 		<!-- Animated Logo -->
 		<figure>
 			<section class="img-bg" />
@@ -20,7 +20,7 @@
 		<!-- / -->
 		<div class="flex justify-center space-x-2">
 			<a
-				class="btn variant-filled"
+				class="btn bg-red-900 hover:bg-orange-600 text-blue-100 font-bold"
 				href="https://skeleton.dev/"
 				target="_blank"
 				rel="noreferrer"
