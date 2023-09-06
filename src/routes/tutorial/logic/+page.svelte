4e7d7e1deccd9883fb7a8ab967ca23bc99a73c87
Thor Anderson <ta.anderson@gmail.com>
2023-09-06 15:47:41 -0600

    Added if/else logic blocks

---
+++ src/routes/tutorial/logic/+page.svelte
@@ -0,0 +1,20 @@
+<script lang=ts>
+    let count = 0;
+
+    function increment() {
+        count += 1;
+    }
+
+</script>
+
+<button class="btn btn-lrg variant-filled-primary" on:click={increment}>
+    Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
+
+{#if count > 10}
+    <p>{count} is greater than 10</p>
+{:else if count < 5}
+<p>{count} is less than 5</p>
+{:else}
+    <p>{count} is somewhere between 0 and 10</p>
+{/if}
