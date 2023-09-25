3e5e0fb372af5d0fda5d6a4a8526398b10adacf0
Thor Anderson <ta.anderson@gmail.com>
2023-09-25 15:44:20 -0600

    Signup and login forms

---
+++ src/routes/auth/login/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+
+    const credentials = {
+        email: '',
+        password: ''
+    }
+
+    const handleSubmit = async () => {
+        // contact our auth api to login the user
+       //console.log(credentials)
+    }
+
+</script>
+
+<div class="container h-full mx-auto flex justify-center">
+    <div class="card text-center w-1/3">
+    <form on:submit|preventDefault={handleSubmit}  class="form flex flex-col space-y-10 p-4 bg-slate-600 rounded-lg">
+        <h1 class="text-3xl font-bold">Login</h1>
+
+        <input bind:value={credentials.email} required autocomplete="email" type="email" placeholder="Email" />
+        <input class="text-black" bind:value={credentials.password} required autocomplete="current-password" type="password" placeholder="Password" />
+
+        <button type="submit" class="btn variant-filled-primary">Signup</button>
+    </form>
+</div>
+</div>
--- src/routes/auth/signup/+page.svelte
+++ src/routes/auth/signup/+page.svelte
@@ -1,17 +1,28 @@
 <script lang="ts">
 
+const credentials = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+}
+
+const handleSubmit = async () => {
+    console.log('Thanks for submitting!')
+    console.log(credentials)
+}
+
 </script>
 
-<div class="container h-screen">
-    <h1>Signup</h1>
-    <p>Signup page</p>
-    <div class="card bg-slate-100 w-screen h-screen">
-    <form class="form">
-        <input type="text" placeholder="Username" />
-        <input type="email" placeholder="Email" />
-        <input type="month" placeholder="Birthday" />
-        <input type="password" placeholder="Password" />
-        <input type="password" placeholder="Confirm Password" />
+<div class="container h-full mx-auto flex justify-center">
+    <div class="card bg-slate-100 text-center w-1/3">
+    <form on:submit|preventDefault={handleSubmit}  class="form flex flex-col items-center space-y-10 p-4">
+        <h1 class="text-3xl font-bold">Sign up</h1>
+        <input bind:value={credentials.firstName} required autocomplete="given-name" type="text" placeholder="First name" />
+        <input bind:value={credentials.lastName} required autocomplete="family-name" type="text" placeholder="Last name" />
+        <input bind:value={credentials.email} required autocomplete="email" type="email" placeholder="Email" />
+        <input class="text-black" bind:value={credentials.password} required autocomplete="new-password" type="password" placeholder="Password" />
+        <input class="text-black" required type="password" placeholder="Confirm Password" />
         <button type="submit" class="btn variant-filled-primary">Signup</button>
     </form>
 </div>
