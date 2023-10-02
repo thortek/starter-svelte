ffbe273dd85928fc3fd964787d3758c83eff87c1
Thor Anderson <ta.anderson@gmail.com>
2023-10-02 15:10:15 -0600

    Added tick method example

--- src/routes/tutorial/lifecycle/+page.svelte
+++ src/routes/tutorial/lifecycle/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { tick } from 'svelte';
 
 	let pokemonImageURL: string;
 	let pokeName: string;
@@ -12,6 +13,26 @@
 		pokeName = pokemon.name;
 		pokemonImageURL = pokemon.sprites?.other?.['official-artwork']?.front_default;
 	});
+
+	let text = `Select some text and hit the tab key to toggle uppercase`;
+
+	async function handleKeydown(this: any, event: { key: string; preventDefault: () => void; }) {
+		if (event.key !== 'Tab') return;
+
+		event.preventDefault();
+
+		const { selectionStart, selectionEnd, value } = this;
+		const selection = value.slice(selectionStart, selectionEnd);
+
+		const replacement = /[a-z]/.test(selection) ? selection.toUpperCase() : selection.toLowerCase();
+
+		text = value.slice(0, selectionStart) + replacement + value.slice(selectionEnd);
+
+		await tick();
+		this.selectionStart = selectionStart;
+		this.selectionEnd = selectionEnd;
+	}
+	
 </script>
 
 <div class="flex flex-col items-center justify-center">
@@ -21,4 +42,11 @@
 	{:else}
 		<p>Loading...</p>
 	{/if}
+	<textarea class="textarea text-center mx-auto mt-8 w-96 h-32"
+	value={text}
+	on:keydown={handleKeydown}
+/>
 </div>
+
+
+
