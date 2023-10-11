bd151614e90dce8ad2962fecc3ebf5272f14b84c
Thor Anderson <ta.anderson@gmail.com>
2023-10-11 15:13:20 -0600

    Added ProgressBar value tweening

---
+++ src/routes/tutorial/motion/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+import { tweened } from 'svelte/motion';
+import { cubicOut } from 'svelte/easing';
+import { ProgressBar } from '@skeletonlabs/skeleton';
+
+const progress = tweened(0, {
+    duration: 400,
+    easing: cubicOut
+});
+
+</script>
+
+<ProgressBar class="m-2" value={$progress} max={100} height="h-8" meter="bg-red-500" track="bg-blue-500" rounded="rounded-e"  />
+
+<button class="btn variant-filled" on:click={() => $progress = 0}>
+	0%
+</button>
+
+<button class="btn variant-filled" on:click={() => $progress = 25}>
+	25%
+</button>
+
+<button class="btn variant-filled" on:click={() => $progress = 50}>
+	50%
+</button>
+
+<button class="btn variant-filled" on:click={() => $progress = 75}>
+	75%
+</button>
+
+<button class="btn variant-filled" on:click={() => $progress = 100}>
+	100%
+</button>
