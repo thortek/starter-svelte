8207a5f4926e7b41e208c92b0d8a12e23e1bb8e1
Thor Anderson <ta.anderson@gmail.com>
2023-10-23 15:42:16 -0600

    Got browser ViewTransitions working

--- src/routes/tutorial/+layout.svelte
+++ src/routes/tutorial/+layout.svelte
@@ -1,1 +1,47 @@
+<script lang="ts">
+    import { onNavigate } from "$app/navigation";
+
+    onNavigate((navigation) => {
+       if (!document.startViewTransition) return;
+
+       return new Promise((resolve) => {
+           document.startViewTransition(async () => {
+            resolve();
+            await navigation.complete;
+           });
+       });
+    })
+
+</script>
+
+<header>
+	<ul class="flex gap-4 justify-center font-bold">
+		<li class="btn variant-ghost-primary cursor-pointer">
+			<a href="/tutorial/bindings">Bindings</a>
+		</li>
+		<li class="btn variant-ghost-primary cursor-pointer">
+			<a href="/tutorial/events">Events</a>
+		</li>
+		<li class="btn variant-ghost-primary cursor-pointer">
+			<a href="/tutorial/lifecycle">LifeCycle</a>
+		</li>
+		<li class="btn variant-ghost-primary cursor-pointer">
+			<a href="/tutorial/motion">Motion</a>
+		</li>
+		<li class="btn variant-ghost-primary cursor-pointer">
+			<a href="/tutorial/props">Props</a>
+		</li>
+		<li class="btn variant-ghost-primary cursor-pointer">
+			<a href="/tutorial/reactivity">Reactivity</a>
+		</li>
+		<li class="btn variant-ghost-primary cursor-pointer"><a href="/tutorial/logic">Logic</a></li>
+	</ul>
+</header>
 <slot />
+
+<!-- <style>
+    :root::view-transition-old(root), 
+    :root::view-transition-new(root) {
+    animation-duration: 1s;
+}
+</style> -->
