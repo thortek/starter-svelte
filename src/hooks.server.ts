d1f75dcd3234cc341d00868beaaab835593a557a
Thor Anderson <ta.anderson@gmail.com>
2023-11-01 14:46:59 -0600

    Failed sign in using Google as well

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,11 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
 import GitHub from "@auth/core/providers/github"
-import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
+import Google from "@auth/core/providers/google"
+import { GITHUB_ID, GITHUB_SECRET, GOOGLE_ID, GOOGLE_SECRET } from "$env/static/private"
 
 export const handle = SvelteKitAuth({
-  providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+  providers: [
+    GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }),
+    Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })
+  ],
 })
--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -6,8 +6,7 @@
   {#if $page.data.session?.user}
     <p>Signed in as {$page.data.session.user.email}</p>
     <button on:click={signOut}>Sign out</button>
-    <img src="https://cdn.pixabay.com/photo/2017/08/11/19/36/vw-2632486_1280.png" />
   {:else}
     <p>Not signed in.</p>
-    <button on:click={() => signIn('github')}>Sign in</button>
+    <button on:click={() => signIn('google')}>Sign in</button>
   {/if}
