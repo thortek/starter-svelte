5087f49733dcd76f88631876eea51b8605295039
Thor Anderson <ta.anderson@gmail.com>
2023-11-08 15:33:40 -0700

    Added production env values

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,12 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
 import GitHub from "@auth/core/providers/github"
 import Google from "@auth/core/providers/google"
-import { GITHUB_ID, GITHUB_SECRET, GOOGLE_ID, GOOGLE_SECRET } from "$env/static/private"
+import { GITHUB_ID, GITHUB_SECRET, GOOGLE_ID, GOOGLE_SECRET, GITHUB_PROD_ID, GITHUB_PROD_SECRET } from "$env/static/private"
 
 export const handle = SvelteKitAuth({
   providers: [
     GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }),
-    Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })
+    Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET }),
+    GitHub({ clientId: GITHUB_PROD_ID, clientSecret: GITHUB_PROD_SECRET }),
   ],
 })
