33ec52b478387466eb5b86af55190056c8966a49
Thor Anderson <ta.anderson@gmail.com>
2023-11-13 15:08:06 -0700

    GitHub Auth working locally and in prod

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,7 +6,7 @@ import Google from "@auth/core/providers/google"
 let githubConfig = { clientId: process.env.GITHUB_PROD_ID, clientSecret: process.env.GITHUB_PROD_SECRET }
 
 if (process.env.NODE_ENV === "development") {
-  githubConfig = { clientId: process.env.GITHUB_LOCAL_ID, clientSecret: process.env.GITHUB_LOCAL_SECRET }
+  githubConfig = { clientId: process.env.GITHUB_DEV_ID, clientSecret: process.env.GITHUB_DEV_SECRET }
 }
 
 export const handle = SvelteKitAuth({
